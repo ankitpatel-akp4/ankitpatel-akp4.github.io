9509faaf31b5dcac472bbea908e1428753d3f011
indicate0 <indicate0@gmail.com>
2023-03-04 02:50:28 +0530

    .

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 let menu_lable = document.getElementById("menu_label");
 let nav_links = document.getElementById("nav_links");
+let animate = document.getElementById("animate");
+// {/* <span><</span>Ankit <span>Patel</span>> */}
+let span_left = document.createElement("span")
+let span_right = document.createElement("span")
+span_left.setAttribute("id","left_span")
+span_right.setAttribute("id","right_span")
+let animate_content = ["<","A","n","k","i","t ","P","a","t","e","l",">"]
+let i = 0
+animate.append(span_left)
+span_left.innerText=animate_content[i++]
+
+setInterval(() => {
+    if(i == animate_content.length){
+        i = 0
+        animate.innerHTML = null
+        span_left.innerText=null
+        span_right.innerText=null
+    }
+    if(i == 0){
+        animate.append(span_left)
+        span_left.innerText=animate_content[i]
+    } 
+    else if(i>0 && i<=5){
+        animate.innerHTML+=animate_content[i]
+        
+    }else
+    if(i>=6 && i<=animate_content.length-2){
+        if(i == 6) animate.append(span_right)
+        span_right.innerText+=animate_content[i]
+    }else{
+        animate.innerHTML+=animate_content[i]
+    }
+    i++
+    
+}, 300);
+
 
 menu_lable.addEventListener("click",()=>{
     if(nav_links.className == "nav_links_visible"){
@@ -19,4 +55,5 @@ nav_links.addEventListener("click",()=>{
          menu_lable.innerText = "Menu"
      }
 })
+
 
